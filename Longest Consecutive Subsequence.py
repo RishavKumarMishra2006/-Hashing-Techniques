342e3f0a4ee6a08991da9ad063ca6c2ff8362149
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2026-03-26 02:29:00 +0530

    Create Longest Consecutive Subsequence.py

---
+++ Longest Consecutive Subsequence.py
@@ -0,0 +1,16 @@
+def longest_consecutive(arr):
+    s = set(arr)
+    longest = 0
+
+    for num in s:
+        if num - 1 not in s:
+            current = num
+            length = 1
+
+            while current + 1 in s:
+                current += 1
+                length += 1
+
+            longest = max(longest, length)
+
+    return longest
